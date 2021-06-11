dfd632253a21b5d97b2e8ac3c5537dae4b3edee8
FarhanArdiansyah1 <73591110+FarhanArdiansyah1@users.noreply.github.com>
2021-06-12 01:11:51 +0700

    Modul A
    Upload via web karena git nya error

---
+++ Project2-Farhan-XIRPL1/src/main/java/gradleproject1/DbCon.java
@@ -0,0 +1,26 @@
+package gradleproject1;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author asus
+ */
+ public class DbCon {
+    public static Connection getConn(String host, String port, String username, String password, String dataBaseName) {
+        String configJdbc = "jdbc:mysql://" + host + ":" + port + "/" + dataBaseName;
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(configJdbc,
+            username, password);
+            System.out.println("Koneksi Berhasil");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Koneksi Database Error");
+            conn = null;
+        }
+        return conn;
+    }
+}
